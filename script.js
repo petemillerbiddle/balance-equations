2a51fd2ee987882e2780d4ba514a6f82c53ea58a
petemillerbiddle <petemillerbiddle@gmail.com>
2022-07-04 17:11:00 -0400

    Write TODOs for class methods

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-class Substance {
-    constructor(elements, isReactant, coefficient = 1) {
-        this.elements = elements; //array of element objects
-        this.isReactant = isReactant; //boolean
-        this.coefficient = coefficient; //integer
-    }
-}
-
 class Element {
-    constructor(element, subscript) {
+    constructor(element, subscript = 1) {
         this.element = element;
         this.subscript = subscript;
     }
 }
 
-class Equation {
-    substances = [];
+class Substance {
+    constructor(elements, isReactant, coefficient = 1) {
+        this.elements = elements;
+        this.isReactant = isReactant;
+        this.coefficient = coefficient;
+    }
+    countAtoms() {
+        //TODO take element as arg?
+        //could be in substance multiple times
+    }
 }
 
+class Equation {
+    constructor(substances) {
+        this.substances = substances
+    }
+    isBalanced() {
+        //TODO write logic here
+        console.log('return boolean = ifbalanced')
+    }
+    balanceEquation() {
+        //TODO write balancing algo
+    }
+}
 
 let elem1 = new Element('O', 2);
 let elem2 = new Element('H', 2);
 let elem3 = new Element('H', 2);
-let elem4 = new Element('O', 1);
+let elem4 = new Element('O');
 
 let subst1 = new Substance([elem1], true);
 let subst2 = new Substance([elem2], true);
 let subst3 = new Substance([elem3, elem4], false);
 
-let eqn = new Equation();
-eqn.substances = [subst1, subst2, subst3];
+let eqn = new Equation([subst1, subst2, subst3]);
 
 console.log(eqn);
 console.log(eqn.substances[2].elements[1].subscript)
+
+eqn.isBalanced();
