c21459713b6a465fd907c680e73574d3636a6533
petemillerbiddle <petemillerbiddle@gmail.com>
2022-07-05 15:39:34 -0400

    Create element set in equation object

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class Element {
-    constructor(element, subscript = 1) {
-        this.element = element;
+    constructor(symbol, subscript = 1) {
+        this.symbol = symbol;
         this.subscript = subscript;
     }
 }
@@ -20,6 +20,31 @@ class Substance {
 class Equation {
     constructor(substances) {
         this.substances = substances
+        this.setOfElements = this.elementSet()
+    }
+    elementSet() {
+        const rctsSet = new Set();
+        const prodSet = new Set();
+        Array.from(this.substances).forEach(substance => {
+            if (substance.isReactant) {
+                substance.elements.forEach(element => rctsSet.add(element.symbol));
+            }
+            else substance.elements.forEach(element => prodSet.add(element.symbol));
+        });
+           
+        if (this.setsAreEqual(rctsSet, prodSet)) return rctsSet;
+        else {
+            console.log('not same reactancts and products');
+            return false;
+        }
+    }
+    setsAreEqual(r, p) {
+        if (r.size !== p.size) {
+            return false;
+        }
+        return Array.from(r).every(element => {
+            return p.has(element);
+        });
     }
     isBalanced() {
         //TODO write logic here
@@ -27,21 +52,29 @@ class Equation {
     }
     balanceEquation() {
         //TODO write balancing algo
+        //solve system of equations?
     }
 }
 
-let elem1 = new Element('O', 2);
-let elem2 = new Element('H', 2);
-let elem3 = new Element('H', 2);
-let elem4 = new Element('O');
 
-let subst1 = new Substance([elem1], true);
-let subst2 = new Substance([elem2], true);
-let subst3 = new Substance([elem3, elem4], false);
 
-let eqn = new Equation([subst1, subst2, subst3]);
 
-console.log(eqn);
-console.log(eqn.substances[2].elements[1].subscript)
+function test1() {
+    let elem1 = new Element('O', 2);
+    let elem2 = new Element('H', 2);
+    let elem3 = new Element('H', 2);
+    let elem4 = new Element('O');
 
-eqn.isBalanced();
+    let subst1 = new Substance([elem1], true);
+    let subst2 = new Substance([elem2], true);
+    let subst3 = new Substance([elem3, elem4], false);
+
+    let eqn = new Equation([subst1, subst2, subst3]);
+    
+    // console.log(eqn);
+    // console.log(eqn.substances[2].elements[1].subscript);
+    // console.log(eqn.substances[2].isReactant);
+    if(eqn.setOfElements) console.log('passed');
+    else console.log('returned false');
+
+}
